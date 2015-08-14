9e6c1e0caf92bdc980f82e1900d395182dd530eb
Niten <niten2@yandex.ru>
2015-08-14 16:08:36 +0700

    fixed lesson 1

--- triangle.rb
+++ triangle.rb
@@ -1,21 +1,13 @@
-def is_number?(s)
-  s.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true
-end
-
-
 puts "We compute ploschat triangle"
 
 puts "Enter a value A"
-a = gets
+a = gets.chomp.to_i
 puts "Enter a value H"
-h = gets
+h = gets.chomp.to_i
 
-if is_number?(a) && is_number?(h)
-  s = 0.5 * a.to_i * h.to_i
-  puts "Area triangle = #{s}"
+if a == 0 || h == 0
+  puts "Enter correct the number A and H!"
 else
-  puts "Enter the number A and H!"
+  s = 0.5 * a * h
+  puts "Area triangle = #{s}"
 end
-
-
-
