200f2a01949058345283ef87ce6aa5dd52095ba8
Niten <niten2@yandex.ru>
2015-08-22 23:50:35 +0700

    end lesson 2

--- lesson 2/date_number.rb
+++ lesson 2/date_number.rb
@@ -1,3 +1,5 @@
+# Заданы три числа, которые обозначают число, месяц, год (запрашиваем у пользователя). Найти порядковый номер даты, начиная отсчет с начала года. Учесть, что год может быть високосным
+
 months_day = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
 
 puts "Введите день"
@@ -61,60 +63,3 @@ end
 
 
 
-
-# month
-# months_day.index(month)
-# x = 0
-# months_day[month - 1]
-# day = 0
-# x = 0
-# while  month <= 0
-#   day += months_day[x]
-#   x = month - 1
-#   # months_day_count = months_day[month - 1]
-# end
-# p day
-# months_count = 0
-# months_while = 0
-# months_count_day = 0
-# months_sum_day = 0
-# months_count_day_2 = 0
-# months_name = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
-# while  months_while <= 0
-#   months_while = months_name.index(month) - 1
-#   months_count += months_while
-# end
-# p months_count
-# months_count = months_count_day_2
-# while  months_count_day_2 <= 0
-#   months_sum_day += months_day[months_count_day_2]
-#   months_count_day_2 -= 1
-# end
-# p months_sum_day
-# количество дней
-# t = Time.now
-# day = t.yday # 315
-# проверка на высокосньсть
-# require "date"
-# flag1 = Date.julian_leap? 1700    # true
-# flag2 = Date.gregorian_leap? 1700 # false
-# flag3 = Date.leap? 1700           # false
-#  def Time.leap? Year
-#   if year % 400 == 0
-#    true
-#   elsif year % 100 == 0
-#    false
-#   elsif year % 4 == 0
-#    true
-#   else
-#    false
-#  end
-# end
-# p flag1
-# months_h = { "january" => 31, "february" => 28, "march" => 31, "april" => 30, "may" => 31, "june" => 30, "july" => 31, "august" => 31, "september" => 30, "october" => 31, "november" => 30, "december" => 31 }
-# months.each { |month, days| puts month if days == 30 }
-# months.each_with_index { |letter, index| vowels[letter] = index + 1 if ['a', 'e', 'o', 'u','i'].include?(letter) }
-# p months_h[month]
-# p months_ary.index(month) + 1
-# month_number = months_ary.index(month) + 1
-# all = number +
--- lesson 2/vowel.rb
+++ lesson 2/vowel.rb
@@ -1,48 +1,6 @@
-# alpha = ("a".."z")
-# alpha.each_key do |key|
-#   if ['a', 'e', 'o', 'u','i'].include?(key)
-#     vowels[key]
+# Заполнить хеш гласными буквами, где значением будет являтся порядковый номер буквы в алфавите (a - 1).
 
-
-
-
-
-
-
-
-# alphabet = ('a'..'z')
-# vowels = {}
-# alphabet.each_with_index { |letter, index| vowels[letter] = index + 1 if ['a', 'e', 'o', 'u','i'].include?(letter) }
-# p vowels
-# vowels = {"a": 0, "e": 0, "o": 0, "u": 0, "i": 0}
-# hash = Hash.new
-# %w(a e i o u y).each_with_index {|item, index|
-#   hash[item] = index
-# }
-# p hash
-
-
-# alpha = ("a".."z").to_h
-# a.index("y")
-# p z
-
-
-
-
-# vower = {}
-
-# vower[:a] = 0
-
-
-
-
-#  # A, E, I, O, U, Y.
-# # while
-# p vower
-
-
-
-# alphabet = ('a'..'z')
-# vowels = {}
-# alphabet.each_with_index { |letter, index| vowels[letter] = index + 1 if ['a', 'e', 'o', 'u','i'].include?(letter) }
-# p vowels
+alph = ('a'..'z')
+vowels = {}
+alph.each_with_index { |key, index| vowels[key] = index + 1 if ['a', 'e', 'o', 'u','i'].include?(key) }
+p vowels
